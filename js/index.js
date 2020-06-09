ad359107a201b00ad0e11c432dafce929fb4b784
bhawnishk1 <mrbhawnish@gmail.com>
2020-06-09 16:12:49 -0700

    last MVP met

--- js/index.js
+++ js/index.js
@@ -1,2 +1,75 @@
-// Your code goes here
-// ..
+let txtContent = document.querySelectorAll('.text-content');
+
+//Change color to blue when hovered over both text-content  (MouseOver & mouseout events included)
+Array.from(txtContent).forEach(text => {
+     text.addEventListener('mouseover', function(e){
+        text.style.color= "blue";
+        e.stopPropagation();
+     text.addEventListener('mouseout', function(){
+        text.style.color= "initial";
+       })
+     })
+
+})
+
+//using resize evenet on first image to add opacity
+let imgCont = document.querySelector("img");
+
+window.addEventListener('resize', function(){
+   imgCont.style.opacity += "0.5";
+})
+
+//click event below using stopPropagation to prevent further bubbling
+
+let signUpBtn = document.querySelector('.btn');
+
+
+document.addEventListener('click', function(e){
+   signUpBtn.textContent = "double click me!";
+   signUpBtn.style.background = "black";
+    e.stopPropagation();
+});
+
+
+//double click event below
+signUpBtn.addEventListener('dblclick', function(e){
+    signUpBtn.textContent = "button double clicked!"
+    signUpBtn.style.background = "cyan";
+
+})
+
+// Wheel Event fired while rolling the mouse wheel
+
+let head = document.querySelectorAll("h2");
+Array.from(head).forEach(heading => {
+   heading.addEventListener('wheel', function(e){
+      heading.style.color= "cyan";
+      e.stopPropagation();
+   })
+
+})
+
+//Select second button from content-pick
+
+let contentPick = document.querySelector("h4");
+contentPick.addEventListener('select', function(){
+   contentPick.innerText = "<b> Fun in the Sun </b>"
+})
+
+// stop navigation items from refreshing the page
+
+let navitems = document.getElementsByClassName("nav-link");
+Array.from(navitems).forEach(item => {
+item.addEventListener('click', function (event) {
+   event.preventDefault();
+   event.stopPropagation();
+   return false;
+ });
+});
+
+
+
+
+
+
+
